c562abf477f1658da87c10bd45da640683d7769e
Sergei Ermolaev <sermolaev1986@gmail.com>
2021-07-19 16:05:29 +0200

    added field support on details page

--- src/app/model/country.ts
+++ src/app/model/country.ts
@@ -1,7 +1,20 @@
 export type Country = {
   name: string,
   region: string,
+  subregion: string,
   population: number,
   capital: string,
-  flag: string
+  flag: string,
+  nativeName: string,
+  topLevelDomain: string,
+  languages: Array<Language>,
+  currencies: Array<Currency>
+}
+
+export type Language = {
+  name: string
+}
+
+export type Currency = {
+  name: string
 }

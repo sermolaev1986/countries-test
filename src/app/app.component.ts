3a85cb653364a0c9b4668907fb3c7d49480356aa
Sergei Ermolaev <sermolaev1986@gmail.com>
2021-07-19 15:56:20 +0200

    added theme support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThemeService} from "./theme.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  private currentSchema: string = "dark";
+
+  constructor(private themeService: ThemeService) {
+  }
+
+  toggleScheme() {
+    this.currentSchema = this.currentSchema === "dark" ? "light" : "dark";
+    this.themeService.setTheme(this.currentSchema);
+  }
+
+  ngOnInit(): void {
+    this.themeService.setTheme(this.currentSchema);
+  }
 }
